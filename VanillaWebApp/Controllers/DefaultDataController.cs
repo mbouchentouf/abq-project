5eb816a23a02cf402f03e8e495d63bd6daca547e
massimo.bouchentouf <massimo.bouchentouf@CYGRP-ABQ-MBOUC>
2022-02-09 09:59:23 -0700

    Add project files.

---
+++ VanillaWebApp/Controllers/DefaultDataController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace VanillaWebApp.Controllers
+{
+    public class DefaultDataController : ApiController
+    {
+        public string[] myData = { "Data-1", "Data-2", "Data-3" };
+
+        public string[] getData()
+        {
+            return myData;
+        }
+    }
+}
